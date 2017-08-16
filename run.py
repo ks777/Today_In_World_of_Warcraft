bb322c9038ee68686bccf96d361f07456a3f5ef9
Kevin Sandoval <ks777@scarletmail.rutgers.edu>
2017-08-15 22:56:04 -0400

    still testing
    Though integration appears to be easy. I need to figure a way to print certain rows and for it to change daily based on the day.

--- run.py
+++ run.py
@@ -1,17 +1,20 @@
 #!/usr/bin/env python
 
-import pymysql
+import pymysql, time
+print("Current Date: " + time.strftime("%m/%d/%Y"))
 
 conn = pymysql.connect(host='localhost', port= 3306, user='root', passwd=' ', db='wowfactsheet')
 
 print("connection established")
 cur = conn.cursor()
-facts = cur.execute("SELECT * FROM wowfactsheet.wowfacts")
+facts = cur.execute("SELECT wowinfo FROM wowfacts")
+data = cur.fetchall()
 
+#prints all the information...yet i need to print certain rows.
 print("attempting to print out a row to confirm possibility")
 print(facts)
-for row in cur:
-    print(row)
+#for row in data:
+#   print(row[0], row[1])
 print("process achieved! congrates")
 
 #testing out pymysql and python integration. seems quite fluid
