3446276c8c860e527d857df8e57fe6b486beff8e
Kevin Sandoval <ks777@scarletmail.rutgers.edu>
2017-08-16 21:43:02 -0400

    Python Portion seems to be functional!!!
    Will attempt to apply a flask framework into this assignment as well, to continue learning the framework.

--- run.py
+++ run.py
@@ -2,19 +2,25 @@
 
 import pymysql, time
 print("Current Date: " + time.strftime("%m/%d/%Y"))
+target = int(time.strftime("%d"))
+print(target)
 
 conn = pymysql.connect(host='localhost', port= 3306, user='root', passwd=' ', db='wowfactsheet')
 
 print("connection established")
 cur = conn.cursor()
-facts = cur.execute("SELECT wowinfo FROM wowfacts")
-data = cur.fetchall()
+facts = cur.execute("SELECT wowday, wowinfo FROM wowfacts")
+
+print("attempting to print out a row to confirm possibility")
+for i in range(cur.rowcount):
+    row = cur.fetchone()
+    if row[0] == target:
+        print("\n%s" %(row[1]))
+#    elif row[0] != target:
+ #       print(row)
+  #      print("so at least this works")
 
 #prints all the information...yet i need to print certain rows.
-print("attempting to print out a row to confirm possibility")
-print(facts)
-#for row in data:
-#   print(row[0], row[1])
 print("process achieved! congrates")
 
 #testing out pymysql and python integration. seems quite fluid
