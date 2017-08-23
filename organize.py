5bb81103d8f26562d2af875c1f392d761855546e
Kevin Sandoval <ks777@scarletmail.rutgers.edu>
2017-08-22 20:11:15 -0400

    updated files to try to fix up things
    still doesn`t work in Flask framework. gets None as an output.

--- organize.py
+++ organize.py
@@ -2,17 +2,40 @@
 
 import pymysql, time
 
-def info_creation():
-    return("Current Date: " + time.strftime("%m/%d/%Y"))
-    target = int(time.strftime("%d"))
-    conn = pymysql.connect(host='localhost', port= 3306, user='root', passwd=' ', db='wowfactsheet')
-    cur = conn.cursor()
-    facts = cur.execute("SELECT wowday, wowinfo FROM wowfacts")
+def info_creation(): 
+    try:
+        def telltime():
+            print("Current Date: " + time.strftime("%m/%d/%Y"))
+            print('------it runs through here-------')
+            return("Current Date: " + time.strftime("%m/%d/%Y"))
+        
+        info = []
+        telltime()
+        #accesses the sql database
+        target = int(time.strftime("%d"))
+        conn = pymysql.connect(host='localhost', port= 3306, user='root', passwd='hello', db='wowfactsheet')
+        cur = conn.cursor()
+        facts = cur.execute("SELECT wowday, wowinfo FROM wowfacts")
+        def showinfo(info):
+            print('----------so the program goes through the definitions-------')
+            print(info)
+            return(info)
+        
+        for i in range(cur.rowcount):
+            row = cur.fetchone()
+            if row[0] == target:
+                info = row[1]
+                showinfo(info) #should print(row[1]) to get info
 
-    for i in range(cur.rowcount):
-        row = cur.fetchone()
-        if row[0] == target:
-            return("\n%s" %(row[1]))
+        cur.close()
+        conn.close()
+    except IndexError:  
+        return("its wrong?")
 
-    cur.close()
-    conn.close()
+#prints None...but why? Is it because it is only reading the info_creation definiton?    
+
+
+#current situation: need to reword/reformat code for Flask/website usage.
+#need to also work on website design
+#possible solution: define another variable and run it with the for loop to output each correct row
+    
